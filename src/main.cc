6f59599a00fdd76cad0bd1b87c019461ec19c128
unknown <00091523@uca.edu.sv>
2024-09-05 16:31:58 -0600

    feat:Agregar structs person y event
    Se agrego los structs event y person al main para poder trabajar en las funciones principales del programa

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,27 @@
-#include<iostream>
+#include <iostream>
 
-int main(int argc, char*argv[]){
+struct Event
+{
 
+    std::string name;
+    std::string start_date;
+    std::string finish_date;
+    int max_capacity;
+    int registration_list;
+};
+
+struct Person
+{
+
+    std::string name;
+    std::string last_name;
+    std::string discovery_method;
+    int age;
+    std::string career_interest;
+};
+
+int main(int argc, char *argv[])
+{
 
     return 0;
 }
